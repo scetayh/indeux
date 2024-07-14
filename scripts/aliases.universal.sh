5e3b096382a3402675a79e831e71c8d51501d4dc
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-07-14 11:50:35 +0800

    update files

---
+++ scripts/aliases.universal.sh
@@ -0,0 +1,8 @@
+alias l='ls -ahlF --color'
+alias n='uname -a | lolcat && neofetch | lolcat'
+alias v='nano ~/.zshrc ; source ~/.zshrc'
+alias c='cat -n'
+
+alias ssh-rmkh='sudo rm -f ~/.ssh/known_hosts'
+
+alias sudo='sudo '
