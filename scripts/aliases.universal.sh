55033b8996baf3b37bf834ef2880ba5b00186838
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-07-20 20:56:34 +0800

    clear repo

--- scripts/aliases.universal.sh
+++
@@ -1,8 +0,0 @@
-alias l='ls -ahlF --color'
-alias n='uname -a | lolcat && neofetch | lolcat'
-alias v='nano ~/.zshrc ; source ~/.zshrc'
-alias c='cat -n'
-
-alias ssh-rmkh='sudo rm -f ~/.ssh/known_hosts'
-
-alias sudo='sudo '
