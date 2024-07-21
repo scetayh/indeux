cbdf7d5fb6a1aa65970751854116cc464bb44b91
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-07-21 08:27:55 +0800

    plain text index made

--- scripts/indeux.sh
+++ scripts/indeux.sh
@@ -4,10 +4,10 @@ set -e
 
 # 遍历“函数的第1个参数”这个目录下的所有目录
 function Indeux_InterateForDirectories() {
-    echo "$1"
-    for i in `ls $1`; do
+    echo "$1"/
+    for i in $(ls "$1"); do
         if [[ -d $1"/"$i ]]; then
-            Indeux_InterateForDirectories $1"/"$i
+            Indeux_InterateForDirectories "$1""/""$i"
         fi
         #echo $1"/"$i
     done
@@ -15,10 +15,10 @@ function Indeux_InterateForDirectories() {
 
 # 遍历“函数的第1个参数”这个目录下的所有目录和文件
 function Indeux_InterateForDirectoriesAndFiles() {
-    echo "$1"
-    for i in `ls $1`; do
-        if [[ -d $1"/"$i ]]; then
-            Indeux_InterateForDirectoriesAndFiles $1"/"$i
+    echo "$1"/
+    for i in $(ls "$1"); do
+        if [[ -d "$1""/"$i ]]; then
+            Indeux_InterateForDirectoriesAndFiles "$1""/""$i"
         fi
         #echo $1"/"$i
     done
@@ -37,21 +37,25 @@ mkdir -p ./.indeux/
 find . -name 'index.html' -type f -print -exec rm -rf {} \; > /dev/null
 
 Indeux_InterateForDirectories . > ./.indeux/directories.txt
-#sed -i '' '1d' ./.indeux/directories.txt # 删除第1行
-sed -i '' 's/.//' ./.indeux/directories.txt # 删除每一行的前1个字符
-sed -i '' 's/.//' ./.indeux/directories.txt # 删除每一行的前1个字符
-#sed -i '' '/^[[:space:]]*$/d' ./.indeux/directories.txt # 删除空行
+sed -i '' 's/..//' ./.indeux/directories.txt # 删除每一行的前1个字符
 
-cat ./.indeux/directories.txt | wc -l
-if [ "$(cat ./.indeux/directories.txt | wc -l)" -gt 0 ]; then
-    for ((i=1; i<=$(cat ./.indeux/directories.txt | wc -l); i++)); do
-        echo -n "Index of /" >> "$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)/index.html"
-        echo -n "$(eval sed -n '${i}p' ./.indeux/directories.txt)" >> "$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)/index.html"
-        echo "/" >> "$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)/index.html"
-        echo "Index of /" > index.html
+wc -l < ./.indeux/directories.txt 
+if [ "$(wc -l < ./.indeux/directories.txt )" -gt 0 ]; then
+    for ((i=1; i<=$(wc -l < ./.indeux/directories.txt ); i++)); do
+        {
+            echo -n "Index of /"
+            echo "$(eval sed -n '${i}p' ./.indeux/directories.txt)"
+            echo
+        } > "$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)/index.html"
+        {
+            echo "Index of /"
+            echo
+        } > index.html
+
         # `$(eval sed -n '${i}p' ./.indeux/directories.txt)'即为第i行的内容
         # `$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)'即为第i行的内容的绝对路径
-        # `$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)'即为第i行的内容的绝对路径(末尾带“/”)
-        #echo $(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)/
+        # `ls $(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)'即为第i行这个目录下的内容
+        ls -1F "$(pwd)"/"$(eval sed -n '${i}p' ./.indeux/directories.txt)"  >> "$(pwd)/$(eval sed -n '${i}p' ./.indeux/directories.txt)/index.html"
+        ls -1F >> index.html
     done
 fi
